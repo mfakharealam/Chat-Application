8122e85ab45d2f817f04b87cf54586b186779af4
MFA571 <fakhar571@gmail.com>
2019-08-10 00:00:44 +0500

    message model for storing data

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def load_last_15_msgs(self):
+        return Message.objects.order_by('-time_stamp').all()[:15]
